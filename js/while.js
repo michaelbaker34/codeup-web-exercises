8814820315a44529f7892949e8c2a31f31ee41ae
Michael Baker <michael.a.baker34@gmail.com>
2020-12-19 19:35:56 -0600

    update while.js with exercise solution

--- js/while.js
+++ js/while.js
@@ -1,5 +1,6 @@
 "use script";
 
+// 1
 function twoSixteenth() {
     var input = 2;
     var count = 1;
@@ -8,4 +9,25 @@ function twoSixteenth() {
         console.log(input *= 2);
         count++;
     }
+}
+
+// 2
+function coneSellCount() {
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    var conesToSell;
+
+    do {
+        conesToSell = Math.floor(Math.random() * 5) + 1;
+        if (conesToSell <= allCones) {
+            console.log(conesToSell + " cones sold.");
+            allCones -= conesToSell;
+        } else {
+            console.log("Cannot sell you "
+                + conesToSell
+                + " I only have "
+                + allCones
+                + ".")
+        }
+        console.log(allCones + " are left.");
+    } while (allCones > 0);
 }
