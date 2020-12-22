2ff4c6d87ccb40b0d67a0eba40d749ca26ec5506
Michael Baker <michael.a.baker34@gmail.com>
2020-12-22 15:53:08 -0600

    update complete planets-array exercise

--- js/planets-array.js
+++ js/planets-array.js
@@ -19,22 +19,40 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift("The Sun");
     console.log(planets);
+
+    console.log("===================================");
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push("Pluto");
     console.log(planets);
+
+    console.log("===================================");
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift("The Sun");
     console.log(planets);
+
+    console.log("===================================");
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop("Pluto");
     console.log(planets);
+
+    console.log("===================================");
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log("Earth in 'planets' array is at index: " + planets.indexOf("Earth"))
 
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
 
+    console.log("===================================");
+
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
+
 })();
