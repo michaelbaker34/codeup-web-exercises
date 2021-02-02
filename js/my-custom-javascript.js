0ce54daa5b78a5fc2f32014009815af18c79fa5d
Michael Baker <michael.a.baker34@gmail.com>
2021-02-02 14:37:18 -0600

    complete jquery exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,3 +1,18 @@
 "use strict";
 
-$(document).ready(() => alert("Dom Loaded"));
+/* alerts */
+// $(document).ready(() => alert("Dom Loaded"));
+// let mainHeadHtml = $("#main-head").html();
+// alert(mainHeadHtml);
+// let paragraphHtml = $("#paragraph").html();
+// alert(paragraphHtml);
+
+/* css borders */
+// let codeupListItem = $(".codeup").css("border", "1px solid #f00")
+
+$("li").css("font-size", "20px");
+
+$("h1, p, li").css("background-color", "#00FFFF")
+
+let headEl = $("h1").text();
+alert(headEl);
