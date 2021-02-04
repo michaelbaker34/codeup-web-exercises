490a6463a7eb0efc083fc5701cb3bd415c10faae
Michael Baker <michael.a.baker34@gmail.com>
2021-02-04 15:49:23 -0600

    complete jquery faq exercice

--- js/jquery_faqs.js
+++ js/jquery_faqs.js
@@ -1,2 +1,14 @@
 "use strict;"
 
+    let button = $(".answerButton");
+    let buttonHandler = function(e) {
+        $("dd").toggleClass("invisible");
+    }
+
+    let title = $("dt");
+    let highlightHandler = function(e) {
+        $(this).toggleClass("highlight")
+    }
+
+    button.click(buttonHandler);
+    title.click(highlightHandler);
