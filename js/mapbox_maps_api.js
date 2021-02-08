466bf716c562649b62a2ff76f2bb89053b483d3e
Michael Baker <michael.a.baker34@gmail.com>
2021-02-08 14:31:19 -0600

    add geocode functions rearrange script load order

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,1 +1,19 @@
 "use strict";
+mapboxgl.accessToken = mapboxToken;
+
+function renderMap() {
+    let map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-74.5, 40],
+        zoom: 8
+    });
+}
+
+function geocodeInput(address) {
+
+}
+
+function reverseGeocodeInput(lat, long) {
+
+}
