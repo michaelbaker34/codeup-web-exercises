e7632f38de61f68ec8d41838469e2cd999f09e90
Michael Baker <michael.a.baker34@gmail.com>
2021-02-08 14:59:27 -0600

    add geocode utils complete geocode and reverse geocode functions

--- js/geocode_utils.js
+++ js/geocode_utils.js
@@ -1,12 +1,11 @@
 "use strict";
+
 mapboxgl.accessToken = mapboxToken;
 
 function renderMap() {
     let map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11",
-        center: [-74.5, 40],
-        zoom: 8
     });
 }
 
