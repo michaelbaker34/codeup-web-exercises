97c2ab088476ec62ea9a37fa857a82d57236eacb
Michael Baker <michael.a.baker34@gmail.com>
2021-02-09 15:47:46 -0600

    add entry to city json complete function handleResponse

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,15 +1,33 @@
 "use strict;"
 
-    // geocode and log san antonio lat/long
-let saCoords = geocode("san antonio, texas 78247", MAPBOX_TOKEN);
-// console.log(saCoords);
+let cityList = $.get("data/city.json");
+let weatherMapUrl = "https://api.openweathermap.org/data/2.5/onecall?";
+let appId = "&appid=" + WEATHERMAP_TOKEN;
+let units = "&units=imperial";
 
-    // get san antonio current temp
-let saCurrentTemp = $.get("https://api.openweathermap.org/data/2.5/onecall?" +
+    // get temperature
+let temp = $.get(weatherMapUrl +
     "lat=29.58&" +
     "lon=-98.39&" +
-    "units=imperial&" +
-    "exclude={part}&" +
-    "appid=" + WEATHERMAP_TOKEN).done(function (data) {
-        console.log(data.current.temp);
+    "units=imperial" +
+    appId).done(function (data) {
+        // console.log(data);
+        // console.log(data.current.temp);
     });
+
+function handleResponse(cities) {
+    let html = "";
+    cities.forEach(function (city) {
+        let temp = 0;
+        let name = city.cityName;
+        let cityHtml = "<h2>City: " + name + "</h2>";
+        cityHtml += "<h4>Temperature: " + temp + "</h4>"
+        html += cityHtml;
+    });
+    $("#current-city").html(html);
+}
+
+cityList.done(handleResponse);
+
+// console.log(geocode("san antonio, texas 78247", MAPBOX_TOKEN));
+// console.log(geocode("bradley beach, new jersey 07720", MAPBOX_TOKEN));
