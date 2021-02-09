6686f4d737ebb989f2d4c8a990bb3b7d1c3b9170
Michael Baker <michael.a.baker34@gmail.com>
2021-02-09 12:37:34 -0600

    add current temp and coords variables for san antonio

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,10 +1,15 @@
 "use strict;"
 
-$.get("https://api.openweathermap.org/data/2.5/onecall?" +
-    "lat={lat}&" +
-    "lon={lon}&" +
+    // geocode and log san antonio lat/long
+let saCoords = geocode("san antonio, texas 78247", MAPBOX_TOKEN);
+// console.log(saCoords);
+
+    // get san antonio current temp
+let saCurrentTemp = $.get("https://api.openweathermap.org/data/2.5/onecall?" +
+    "lat=29.58&" +
+    "lon=-98.39&" +
+    "units=imperial&" +
     "exclude={part}&" +
-    "appid={API key}" +
-    WEATHERMAP_TOKEN).done(function (data) {
-        console.log(data)
+    "appid=" + WEATHERMAP_TOKEN).done(function (data) {
+        console.log(data.current.temp);
     });
