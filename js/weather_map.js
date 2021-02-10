a0bf43e723974f47c6cb3d129bd019b237865819
Michael Baker <michael.a.baker34@gmail.com>
2021-02-10 11:22:28 -0600

    completely refactor weathermap js. rewrite get request and done handler

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,25 +1,33 @@
 "use strict;"
 
-let placesUrl = "data/places.json";
-let weatherMapUrl = "https://api.openweathermap.org/data/2.5/onecall?";
-let appId = "&appid=" + WEATHERMAP_TOKEN;
-let units = "&units=imperial";
+$.get("http://api.openweathermap.org/data/2.5/weather", {
+    APPID: WEATHERMAP_TOKEN,
+    q: "San Antonio, Tx, US",
+    units: "imperial"
+}).done((data) => console.log(data));
 
-function handleResponse(locations) {
-    let html = "";
-    locations.forEach(function (data) {
-        let cityState = data.cityName + ", " + data.stateName;
-        let temp = "Temperature: 0";
-        console.log(cityState);
-        let placeHtml = '<div class="card-body">';
-        placeHtml += '<div class="card-title">' + cityState + '</div>';
-        placeHtml += '<div class="card-body">Current Temp: ' + temp + '</div>'
-        placeHtml += '</div>';
-        html += placeHtml
-    });
-    $("#placesCards").html(html);
-}
-$.get(placesUrl).done(handleResponse);
+// renderHTML using locations.json
+// let weatherMapUrl = "https://api.openweathermap.org/data/2.5/onecall?";
+// let appId = "&appid=" + WEATHERMAP_TOKEN;
+// let units = "&units=imperial";
+// let locationsUrl = "data/locations.json";
+// function handleResponse(locations) {
+//     let html = "";
+//     locations.forEach(function (data) {
+//         let cityState = data.cityName + ", " + data.stateName;
+//         let temp = "0";
+//         let placeHtml = '<div class="card-body">';
+//         placeHtml += '<div class="card-header card-title">' + cityState + '</div>';
+//         placeHtml += '<div class="card-body">Current Temp: ' + temp + '</div>'
+//         placeHtml += '</div>';
+//         html += placeHtml
+//     });
+//     $("#placesCards").html(html);
+// }
+// function handleGetDone() {
+//     $.get(locationsUrl).done(handleResponse);
+// }
+// handleGetDone();
 
 // get san antonio temp based on coords
 // let temp = $.get(weatherMapUrl
@@ -30,7 +38,6 @@ $.get(placesUrl).done(handleResponse);
 //         console.log(data);
 //         console.log(data.current.temp);
 //     });
-
 // console.log(geocode("san antonio, texas 78247", MAPBOX_TOKEN));
 // console.log(geocode("bradley beach, new jersey 07720", MAPBOX_TOKEN));
 // console.log(geocode("rincon, california 92061", MAPBOX_TOKEN));
