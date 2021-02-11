428b0ac6238e50c3002b4e4dd0dcd33984109b03
Michael Baker <michael.a.baker34@gmail.com>
2021-02-11 15:34:57 -0600

    refactor handleWeatherResponse complete weather and description card-text

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,49 +3,49 @@
 let lat = 29.424;
 let lon = -98.495;
 let units = "imperial";
-let appId = WEATHERMAP_TOKEN;
-let weatherUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=${appId}`;
+let weatherUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=${WEATHERMAP_TOKEN}`;
 
 function handleWeatherResponse(data) {
     console.log(data);
     for (let i = 0; i < 5; i++) {
-        console.log("---day "       + i + "---");
-        console.log("day temp: "    + data.daily[i].temp.day);
-        console.log("night temp: "  + data.daily[i].temp.night);
-        console.log("weather: "     + data.daily[i].weather[0].description);
-        console.log("humidity: "    + data.daily[i].humidity);
-        console.log("uv index: "    + data.daily[i].uvi);
-        console.log("wind speed: "  + data.daily[i].wind_speed);
+        let $div = $("<div>")
+                    .addClass("card")
+                    .attr("id", "weather-card");
+        let $h3 = $("<h3>")
+                    .addClass("card-title card-header")
+                    .attr("id","city-state")
+                    .text("searchInput/clickLocation");
+        let $h5Weather = $("<h5>").addClass("card-text").attr("id", "weather")
+                    .text(`Day: ${data.daily[i].temp.day} Night: ${data.daily[i].temp.night}`);
+        let $h5Description = $("<h5>").addClass("card-text").attr("id", "description")
+                    .text(`Description: ${data.daily[i].weather[0].description}`);
+        $div.append([$h3, $h5Weather, $h5Description]);
+        $div.appendTo($(".cardContainer"));
+        // console.log("---day " + i + "---");
+        // console.log("day temp: "    + data.daily[i].temp.day);
+        // console.log("night temp: "  + data.daily[i].temp.night);
+        // console.log("weather: "     + data.daily[i].weather[0].description);
+        // console.log("humidity: "    + data.daily[i].humidity);
+        // console.log("uv index: "    + data.daily[i].uvi);
+        // console.log("wind speed: "  + data.daily[i].wind_speed);
     }
 }
 
-$.get(weatherUrl).done(handleWeatherResponse);
+function handleGetDone() {
+    $.get(weatherUrl).done(handleWeatherResponse);
+}
+
+handleGetDone();
+
+// failed jquery card render
+// let $div = $("<div>").addClass("card").attr("id", "weather-card");
+// let $h3 = $("<h3>").addClass("card-title card-header").attr("id","city-state").text("searchInput/clickLocation");
+// let $h5 = $("h5").addClass("card-text").attr("id", "weather").text("weather api details here");
+//
+// $div.append([$div, $h3, $h5]);
+// $("#weather-card-container").append($div);
 
 // console.log(geocode("san antonio, tx", MAPBOX_TOKEN));
-
-// renderHTML using locations.json
-// let weatherMapUrl = "https://api.openweathermap.org/data/2.5/onecall?";
-// let appId = "&appid=" + WEATHERMAP_TOKEN;
-// let units = "&units=imperial";
-// let locationsUrl = "data/locations.json";
-// function handleResponse(locations) {
-//     let html = "";
-//     locations.forEach(function (data) {
-//         let cityState = data.cityName + ", " + data.stateName;
-//         let temp = "0";
-//         let placeHtml = '<div class="card-body">';
-//         placeHtml += '<div class="card-header card-title">' + cityState + '</div>';
-//         placeHtml += '<div class="card-body">Current Temp: ' + temp + '</div>'
-//         placeHtml += '</div>';
-//         html += placeHtml
-//     });
-//     $("#locationCards").html(html);
-// }
-// function handleGetDone() {
-//     $.get(locationsUrl).done(handleResponse);
-// }
-// handleGetDone();
-
 // get san antonio temp based on coords
 // let temp = $.get(weatherMapUrl
 //     + "lat=29.58"
