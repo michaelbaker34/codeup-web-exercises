a62ad8fa4ef29adcaa5f6f2c772692cee0abc8bc
Michael Baker <michael.a.baker34@gmail.com>
2021-02-11 19:14:41 -0600

    add styling to weather-content

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,12 +1,19 @@
 "use strict;"
 
+// global location/weather data
 let lat = 29.424;
 let lon = -98.495;
 let units = "imperial";
 let weatherUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=${WEATHERMAP_TOKEN}`;
 
+// handle weatherUrl data
 function handleWeatherResponse(data) {
     console.log(data);
+
+// jumbotron
+    let location = "city, state";
+    location = $(".location").text(`Location: ${location}`);
+
     for (let i = 0; i < 5; i++) {
         // credit: for timeConverter function https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript/847196#847196
         let UNIX_timestamp = data.current.dt;
@@ -18,45 +25,40 @@ function handleWeatherResponse(data) {
             let date = a.getDate() + i;
             return `${month} ${date}, ${year}`;
         }
-
-        let $div = $("<div>").addClass("card").attr("id", "weather-card");
-
-        let $h3 = $("<h3>").addClass("card-title card-header").attr("id","city-state")
-            .text(`Date: ${weatherMapTimeConverter(UNIX_timestamp)} searchInput/clickLocation`);
-
-        let $h5Weather = $("<h5>").addClass("card-text").attr("id", "weather")
-            .text(`Day: ${data.daily[i].temp.day}˚F Night: ${data.daily[i].temp.night}˚F`);
-
-        let $h5Description = $("<h5>").addClass("card-text").attr("id", "description")
-            .text(`Description: ${data.daily[i].weather[0].description}`);
-
-        let $h5Humidity = $("<h5>").addClass("card-text").attr("id", "humidity")
-            .text(`Humidity: ${data.daily[i].humidity}`);
-
-        let $h5UvIndex = $("<h5>").addClass("card-text").attr("id", "uvIndex")
-            .text(`UV Index: ${data.daily[i].uvi}`);
-
-        let $h5WindSpeed = $("<h5>").addClass("card-text").attr("id", "windSpeed")
-            .text(`Wind Speed: ${data.daily[i].wind_speed}`);
-
-        let $h5WindDirection = $("<h5>").addClass("card-text").attr("id", "windDirection")
-            .text(`Wind Direction: ${data.daily[i].wind_deg}`);
-
-        $div.append([$h3, $h5Weather, $h5Description, $h5Humidity, $h5UvIndex, $h5WindSpeed, $h5WindDirection]);
+// html variables
+        let date = weatherMapTimeConverter(UNIX_timestamp);
+        let dayTemp = Math.round(data.daily[i].temp.day);
+        let nightTemp = Math.round(data.daily[i].temp.night);
+        let description = data.daily[i].weather[0].description;
+        let humidity = data.daily[i].humidity;
+        let uvIndex = data.daily[i].uvi;
+        let windSpeed = Math.round(data.daily[i].wind_speed);
+        let windDir = data.daily[i].wind_deg;
+// format html variables
+        let $div = $("<div>").addClass("col-2 card weather-card");
+        let $h6Date = $("<h6>").addClass("card-title card-header date").text(`Date: ${date}`);
+        let $pWeather = $("<p>").addClass("card-text weather").text(`Day: ${dayTemp}˚F Night: ${nightTemp}˚F`);
+        let $pHumidity = $("<p>").addClass("card-text humidity").text(`Humidity: ${humidity}`);
+        let $pUvIndex = $("<p>").addClass("card-text uvIndex").text(`UV Index: ${uvIndex}`);
+        let $pWindSpeed = $("<p>").addClass("card-text windSpeed").text(`Wind Speed: ${windSpeed} kts`);
+        let $pWindDir = $("<p>").addClass("card-text windDir").text(`Wind Direction: ${windDir}˚`);
+        let $pDescription = $("<p>").addClass("card-text description").text(`Description: ${description}`);
+// render html variables
+        $div.append([$h6Date, $pWeather, $pHumidity, $pUvIndex, $pWindSpeed, $pWindDir, $pDescription]);
         $div.appendTo($(".card-container"));
     }
 }
 
+// handle & call weather url handler function
 function handleGetDone() {
     $.get(weatherUrl).done(handleWeatherResponse);
 }
-
 handleGetDone();
 
-console.log(geocode("san antonio, tx", MAPBOX_TOKEN));
-// get san antonio temp based on coords
-let temp = $.get(weatherMapUrl + "lat=29.58" + "&lon=-98.39" + units + appId)
-    .done(function (data) {
-        console.log(data);
-        console.log(data.current.temp);
-    });
+// console.log(geocode("san antonio, tx", MAPBOX_TOKEN));
+// // get san antonio temp based on coords
+// let temp = $.get(weatherMapUrl + "lat=29.58" + "&lon=-98.39" + units + appId)
+//     .done(function (data) {
+//         console.log(data);
+//         console.log(data.current.temp);
+//     });
