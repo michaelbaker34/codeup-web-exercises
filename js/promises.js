17f888755560bab0e8f2bd93868c12fc7569eb08
Michael Baker <michael.a.baker34@gmail.com>
2021-02-24 12:55:50 -0600

    complete wait function for promises exercise

---
+++ js/promises.js
@@ -0,0 +1,17 @@
+"use-strict";
+
+    function wait(n) {
+        return new Promise((resolve, reject) => {
+            (n > 0) ? resolve(setTimeout(() =>
+                        console.log(`${n} milliseconds have passed`)), n) :
+                      reject(console.error("n 0 or less "));
+        });
+    }
+    wait(3000).then((message) => console.log(message))
+                 .catch((message) => console.error(message));
+
+    wait(1000).then((message) => console.log(message))
+                 .catch((message) => console.error(message));
+
+// fetch(url, {headers: {"Authorization": GITHUB_TOKEN}});
+
