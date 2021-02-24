c0abedd92962d098f010e983abc6b5c8646ab9ee
Michael Baker <michael.a.baker34@gmail.com>
2021-02-24 09:13:26 -0600

    reformat spacing

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -42,50 +42,49 @@ const users = [
 *   use .filter() create array of user objects
 *   each user object has at least 3 languages in array
 */
+    let languages = users.filter(x => x.languages.length >= 3);
 
-let languages = users.filter(x => x.languages.length >= 3);
-console.log("--- languages ---");
-console.log(languages);
+    console.log("--- languages ---");
+    console.log(languages);
 
 /* TODO
 *   use .map() create array of strings
 *   each element should be email address
 */
+    let emails = users.map(x => x.email);
 
-let emails = users.map(x => x.email);
-console.log("--- emails ---");
-console.log(emails);
+    console.log("--- emails ---");
+    console.log(emails);
 
 /* TODO
 *   use .reduce() get total years exp from users
 *   calculate results of avg
  */
+    let experience = users.reduce((total, x) => (total + x.yearsOfExperience), 0);
+    experience = experience / users.length;
 
-let experience = users.reduce((total, x) => (total + x.yearsOfExperience), 0);
-experience = experience / users.length;
-console.log("--- average years experience ---");
-console.log(experience);
+    console.log("--- average years experience ---");
+    console.log(experience);
 
 /* TODO
 *   use .reduce() get longest email
  */
+    let longestInstructorEmail = users.reduce((longestEmail, userEmail) =>
+        (userEmail.email.length > longestEmail.email.length) ? userEmail : longestEmail);
 
-let longestInstructorEmail = users.reduce((longestEmail, userEmail) =>
-    (userEmail.email.length > longestEmail.email.length) ? userEmail : longestEmail);
-console.log(longestInstructorEmail.email);
+    console.log(longestInstructorEmail.email);
 
 /* TODO
 *   use .reduce() get list of user names in single string
 *   ex: "Your instructors are: name, name, name.
  */
+    let namesArray = users.reduce((a, user) => a.concat(user.name), []);
+    let namesString = `Your instructors are: ${namesArray.join(", ")}`;
 
-let namesArray = users.reduce((a, user) => a.concat(user.name), []);
-let namesString = `Your instructors are: ${namesArray.join(", ")}`;
-console.log(namesString);
+    console.log(namesString);
 
 /* TODO
 *   use .reduce() to get unique languages from user list
  */
-
-// let uniqueLanguages = users.reduce((a, user) => { });
-// console.log(uniqueLanguages);
+    // let uniqueLanguages = users.reduce((a, user) => { });
+    // console.log(uniqueLanguages);
