ca2dd1373c2a07af66f8da2a324f4c89a988f942
Michael Baker <michael.a.baker34@gmail.com>
2021-02-25 14:08:03 -0600

    add jquery to promises html console log self commits with github api

--- js/promises.js
+++ js/promises.js
@@ -5,12 +5,16 @@
             (n > 0) ? resolve(setTimeout(() => { console.log(`${n} milliseconds have passed`); }, n)) : reject(console.error("n 0 or less "));
         });
     }
+    // wait(3000).then((message) => console.log(message))
+    //              .catch((message) => console.error(message));
+    //
+    // wait(1000).then((message) => console.log(message))
+    //              .catch((message) => console.error(message));
 
-    wait(3000).then((message) => console.log(message))
-                 .catch((message) => console.error(message));
+function listCommits(userName) {
+    fetch(`https://api.github.com/users/${userName}/events/public`)
+        .then(response => console.log(response.json()))
+        .catch(error => console.error(error));
+}
 
-    wait(1000).then((message) => console.log(message))
-                 .catch((message) => console.error(message));
-
-// fetch(url, {headers: {"Authorization": GITHUB_TOKEN}});
-
+listCommits("michaelbaker34");
