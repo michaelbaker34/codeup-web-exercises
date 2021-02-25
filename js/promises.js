f83bb35f53727422c7a7b7eb0b46d653d810b4c0
Michael Baker <michael.a.baker34@gmail.com>
2021-02-25 09:46:20 -0600

    refactor wait function

--- js/promises.js
+++ js/promises.js
@@ -2,11 +2,10 @@
 
     function wait(n) {
         return new Promise((resolve, reject) => {
-            (n > 0) ? resolve(setTimeout(() =>
-                        console.log(`${n} milliseconds have passed`)), n) :
-                      reject(console.error("n 0 or less "));
+            (n > 0) ? resolve(setTimeout(() => { console.log(`${n} milliseconds have passed`); }, n)) : reject(console.error("n 0 or less "));
         });
     }
+
     wait(3000).then((message) => console.log(message))
                  .catch((message) => console.error(message));
 
